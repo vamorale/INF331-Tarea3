da18e3bf340d9e4358f9659f86cf90cdeddcaf53
vamorale <87721403+vamorale@users.noreply.github.com>
2023-07-05 22:37:46 -0400

    Init pruebas, update readme

--- Code/inventory.py
+++ Code/inventory.py
@@ -1,5 +1,6 @@
 import funciones
 from datetime import datetime
+import os
 
 games = dict()
 sales = []
@@ -7,6 +8,8 @@ purchases = []
 
 platforms = ["PC", "Switch", "Xbox Series", "PS5"]
 genres = ["Acción","Aventura","Arcade","Deportes","Estrategia","Simulación","Juegos de mesa","Juegos musicales"]
+
+#Mostrar el catálogo en consola
 
 def catalog():
     if len(games.keys())!=0:
@@ -21,14 +24,17 @@ def catalog():
     else:
         print("No hay juegos en catálogo.")
 
+#Listar los juegos al momento de comprar por parte del cliente
+
 def show():
     juegos = games.values()
     lista = []
     for i in juegos:
         lista.append(i[0]+", Precio: "+i[3]+", Cantidad disponible: "+i[-1])
     print(lista,games)
-    #return (lista if len(lista)!=0 else None)
     return lista
+
+# Compra que realiza el administrador
 
 def buy():
     id = len(games.keys()) if len(games.keys())!=0 else 0
@@ -38,9 +44,12 @@ def buy():
     Genre = funciones.listOptions(genres,"Escoge el género del juego:\n")
     Platform = funciones.listOptions(platforms,"Escoge la plataforma del juego:\n")
     cant = input("Ingresa la cantidad a comprar: ")
-    games[id] = [title,Bprice,Sprice,Genre,Platform,cant]
+    print(genres[int(Genre)-1],platforms[int(Platform)-1])
+    games[id] = [title,Bprice,Sprice,genres[int(Genre)-1],platforms[int(Platform)-1],cant]
     purchases.append([id,title,Bprice,cant])
     print("Juego registrado en el inventario exitosamente")
+
+# Ventas del admin y compras del cliente
 
 def sale():
     game = 0
@@ -60,10 +69,11 @@ def sale():
             print(cant)
         else:
             games[int(game)-1][-1] = str(int(games[int(game)-1][-1]) - cant)
-            print("Juego registrado en el inventario exitosamente\n")
+            print("Juego comprado exitosamente.")
             break
     sales.append([int(game)-1,games[int(game)-1][0],games[int(game)-1][2],cant])
-    print(sales)
+
+# Guardar los datos en los archivos
 
 def guardar():
     f = open("inventario.txt","w")
@@ -79,20 +89,26 @@ def guardar():
         f.write(",".join([str(i) for i in valor])+"\n")
     f.close()
 
+# Carga los datos de los archivos en las estructuras de datos
+
 def cargar():
-    f=open("inventario.txt","r")
-    for linea in f:    
-        games[int(linea[0])] = linea[2:].strip().split(",")
-        print("games",games)
-    f.close()
-    f=open("ventas.txt","r")
-    for linea in f:
-        sales.append(linea.strip().split(","))
-    f.close()
-    f=open("compras.txt","r")
-    for linea in f:
-        purchases.append(linea.strip().split(","))
-    f.close()
+    if os.stat("inventario.txt").st_size != 0:
+        f=open("inventario.txt","r")
+        for linea in f:    
+            games[int(linea[0])] = linea[2:].strip().split(",")
+        f.close()
+    if os.stat("ventas.txt").st_size != 0:
+        f=open("ventas.txt","r")
+        for linea in f:
+            sales.append(linea.strip().split(","))
+        f.close()
+    if os.stat("compras.txt").st_size != 0:
+        f=open("compras.txt","r")
+        for linea in f:
+            purchases.append(linea.strip().split(","))
+        f.close()
+
+# Genera reporte de desempeño
 
 def reporte():
     n_compras=0
@@ -122,7 +138,7 @@ def reporte():
     f.write("Cantidad total de compras: "+str(n_compras)+". Cantidad total de ventas: "+str(n_ventas)+".\n")
     f.write("Cantidad total de juegos comprados: "+str(n_juegos_c)+". Cantidad total de juegos vendidos: "+str(n_juegos_v)+".\n")
     f.write("Egresos: "+str(total_compras)+". Ingresos: "+str(total_ventas)+". Ganancia total: "+ str(total_ventas-total_compras)+".")
-
     c.close()
     v.close()
     f.close()
+    print("Reporte generado exitosamente.")
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # INF331-Tarea3
 Desarrollo y pruebas de un simulador de gestión de una tienda de videojuegos
+
+Víctor Morales Alanis
+
+## Supuestos:
+
+- La compra de parte del administrador se refiere a las compras que hace a las distribuidoras de videojuegos.
+- Por lo anterior, es que para agregar elementos al inventario se hace en la sección de compra.
+- Las ventas del administrador son equivalente a las compras que realiza el cliente en su sección.
+- No se requiere ingresar contraseña para ningún perfil.
+- El precio de compra es el precio que el administrador paga a las distribuidoras por los juegos, mientras que el de venta es el que el cliente paga al comprar un juego.
+
+## Ejecución
+
+- Para correr las pruebas se debe ejecutar el siguiente comando:
+
+py -m unittest pruebas.py o python -m unittest pruebas.py
+
+- Para ejecutar el simulador se utiliza el siguiente comando:
+
+py main.py o python main.py
