b8c9f51fde82201deec38f8a83ffc00b2082caf3
vamorale <87721403+vamorale@users.noreply.github.com>
2023-07-05 22:03:09 -0400

    Added "reporte" and more

---
+++ Code/inventory.py
@@ -0,0 +1,128 @@
+import funciones
+from datetime import datetime
+
+games = dict()
+sales = []
+purchases = []
+
+platforms = ["PC", "Switch", "Xbox Series", "PS5"]
+genres = ["Acción","Aventura","Arcade","Deportes","Estrategia","Simulación","Juegos de mesa","Juegos musicales"]
+
+def catalog():
+    if len(games.keys())!=0:
+        for game in games.values():
+            print("Título: "+game[0]+
+                ". Precio compra: "+game[1]+
+                ". Precio venta: "+game[2]+
+                ". Género: "+game[3]+
+                ". Plataforma: "+game[4]+
+                ". Cantidad disponible: "+game[-1]+
+                ".")
+    else:
+        print("No hay juegos en catálogo.")
+
+def show():
+    juegos = games.values()
+    lista = []
+    for i in juegos:
+        lista.append(i[0]+", Precio: "+i[3]+", Cantidad disponible: "+i[-1])
+    print(lista,games)
+    #return (lista if len(lista)!=0 else None)
+    return lista
+
+def buy():
+    id = len(games.keys()) if len(games.keys())!=0 else 0
+    title = input("Ingresa el título del juego: ")
+    Bprice = input("Ingresa el precio de compra: ")
+    Sprice = input("Ingresa el precio de venta: ")
+    Genre = funciones.listOptions(genres,"Escoge el género del juego:\n")
+    Platform = funciones.listOptions(platforms,"Escoge la plataforma del juego:\n")
+    cant = input("Ingresa la cantidad a comprar: ")
+    games[id] = [title,Bprice,Sprice,Genre,Platform,cant]
+    purchases.append([id,title,Bprice,cant])
+    print("Juego registrado en el inventario exitosamente")
+
+def sale():
+    game = 0
+
+    while True:
+        game = funciones.listOptions(list(show()),"Selecciona un juego:\n")
+        if int(games[int(game)-1][-1]) == 0:
+            print("Este juego está agotado")
+        else:
+            break
+
+    while True:
+        cant = 0
+        cant = int(input("Ingresa la cantidad: "))
+        if cant>int(games[int(game)-1][-1]) or cant<=0:
+            print("Esa cantidad no es válida")
+            print(cant)
+        else:
+            games[int(game)-1][-1] = str(int(games[int(game)-1][-1]) - cant)
+            print("Juego registrado en el inventario exitosamente\n")
+            break
+    sales.append([int(game)-1,games[int(game)-1][0],games[int(game)-1][2],cant])
+    print(sales)
+
+def guardar():
+    f = open("inventario.txt","w")
+    for i,valor in games.items():
+        f.write(str(i)+","+",".join(valor)+"\n")
+    f.close()
+    f = open("ventas.txt","w")
+    for valor in sales:
+        f.write(",".join([str(i) for i in valor])+"\n")
+    f.close()
+    f = open("compras.txt","w")
+    for valor in purchases:
+        f.write(",".join([str(i) for i in valor])+"\n")
+    f.close()
+
+def cargar():
+    f=open("inventario.txt","r")
+    for linea in f:    
+        games[int(linea[0])] = linea[2:].strip().split(",")
+        print("games",games)
+    f.close()
+    f=open("ventas.txt","r")
+    for linea in f:
+        sales.append(linea.strip().split(","))
+    f.close()
+    f=open("compras.txt","r")
+    for linea in f:
+        purchases.append(linea.strip().split(","))
+    f.close()
+
+def reporte():
+    n_compras=0
+    n_juegos_c=0
+    total_compras=0
+    n_ventas=0
+    n_juegos_v=0
+    total_ventas=0
+    f=open("reporte_"+str(datetime.now().strftime('%Y-%m-%d_%H.%M.%S'))+".txt","w")
+    v=open("ventas.txt","r")
+    c=open("compras.txt","r")
+    f.write("Reporte de ventas\n---------------------\nCompras:\n")
+    for i in c:
+        linea=i.strip().split(",")
+        total_compras+=int(linea[2])*int(linea[3])
+        n_compras+=1
+        n_juegos_c+=int(linea[3])
+        f.write("- Juego: "+linea[1]+", precio: "+linea[2]+", cantidad: "+linea[3]+"\n")
+    f.write("---------------------\nVentas:\n")
+    for i in v:
+        linea=i.strip().split(",")
+        total_ventas+=int(linea[2])*int(linea[3])
+        n_ventas+=1
+        n_juegos_v+=int(linea[3])
+        f.write("- Juego: "+linea[1]+", precio: "+linea[2]+", cantidad: "+linea[3]+"\n")
+    f.write("---------------------\nResultados:\n")
+    f.write("Cantidad total de compras: "+str(n_compras)+". Cantidad total de ventas: "+str(n_ventas)+".\n")
+    f.write("Cantidad total de juegos comprados: "+str(n_juegos_c)+". Cantidad total de juegos vendidos: "+str(n_juegos_v)+".\n")
+    f.write("Egresos: "+str(total_compras)+". Ingresos: "+str(total_ventas)+". Ganancia total: "+ str(total_ventas-total_compras)+".")
+
+    c.close()
+    v.close()
+    f.close()
--- Code/main.py
+++ Code/main.py
@@ -1,25 +1,34 @@
 import funciones
+import inventory
 
 def AdminMode():
-    opciones= ["Comprar juego", "Vender juego", "Generar reporte de desempeño", "Cerrar sesión"]
-    opcion = funciones.listOptions(opciones,"Selecciona una opción:\n")
-    if opcion == "1":
-        print("compra")
-    elif opcion == "2":
-        print("venta")
-    elif opcion == "3":
-        print("reporte")
-    elif opcion == "4":
-        print(funciones.lista)
-        exit(0)
+    opciones= ["Ver catálogo","Comprar juego", "Vender juego", "Generar reporte de desempeño", "Cerrar sesión"]
+    while True:
+        opcion = funciones.listOptions(opciones,"Selecciona una opción:\n")
+        if opcion == "1":
+            inventory.catalog()
+        elif opcion == "2":
+            inventory.buy()
+        elif opcion == "3":
+            inventory.sale()
+        elif opcion == "4":
+            inventory.guardar()
+            inventory.reporte()
+        elif opcion == "5":
+            break
 
 def ClientMode():
-    opciones= ["Comprar juego", "Cerrar sesión"]
-    opcion = funciones.listOptions(opciones,"Selecciona una opción:\n")
-    if opcion == "1":
-        print("compra")
-    elif opcion == "2":
-        exit(0)
+    opciones= ["Ver catálogo","Comprar juego", "Cerrar sesión"]
+    while True:
+        opcion = funciones.listOptions(opciones,"Selecciona una opción:\n")
+        if opcion == "1":
+            inventory.catalog()
+        elif opcion == "2":
+            inventory.sale()
+        elif opcion == "3":
+            break
+
+inventory.cargar()
 
 print("""
 -------------------------------------
@@ -27,11 +36,15 @@ print("""
 -------------------------------------
 """)
 
-typeUser=["Administrador","Cliente"]
+typeUser=["Iniciar sesión como administrador","Iniciar sesión como cliente","Salir"]
 
 while True:
-    user = funciones.listOptions(typeUser,"Iniciar sesión como:\n")
+    user = funciones.listOptions(typeUser,"Elige una opción:\n")
     if user == "1":
         AdminMode()
     elif user == "2":
         ClientMode()
+    elif user == "3":
+        inventory.guardar()
+        print("¡Hasta luego!")
+        exit(0)
